6ed51a11f8eeb9a70dfe65948404a467dcbb7867
Htet Aung Lin <halucsmub@gmail.com>
2018-10-16 22:36:50 +0630

    update commit

--- app/src/main/java/htetaunglin/com/materialtab/fragment/FirstFragment.kt
+++ app/src/main/java/htetaunglin/com/materialtab/fragment/FirstFragment.kt
@@ -1,7 +1,16 @@
 package htetaunglin.com.materialtab.fragment
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import htetaunglin.com.materialtab.R
 
-class FirstFragment: Fragment() {
+class FirstFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.first_fragment, container, false)
+        return view
+    }
 
 }
--- app/src/main/java/htetaunglin/com/materialtab/fragment/SecondFragment.kt
+++ app/src/main/java/htetaunglin/com/materialtab/fragment/SecondFragment.kt
@@ -1,6 +1,15 @@
 package htetaunglin.com.materialtab.fragment
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import htetaunglin.com.materialtab.R
 
 class SecondFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.second_fragment, container, false)
+        return view
+    }
 }
